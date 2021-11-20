afba5cd0f854e2434f6b8d5bbde9ec015e159345
nazar1881 <90273320+nazar1881@users.noreply.github.com>
2021-11-20 12:41:43 +0200

    last change

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 let elem = document.getElementById("figur");
 let select = document.getElementById("my-select");
 
+let color = document.getElementById("inputColor");
+let btn = document.getElementById("my-btn");
+
 select.addEventListener("change", function(){
     elem.classList = select.value;   
 });
-
-let color = document.getElementById("inputColor");
-let btn = document.getElementById("my-btn");
 
 btn.addEventListener("click", function(){
     if (!color.value) {
